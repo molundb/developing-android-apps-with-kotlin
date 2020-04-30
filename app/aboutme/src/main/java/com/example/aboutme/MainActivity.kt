91df5c70111f982d0eb96f125e7bf73273b681a6
Lundberg, Martin (Extern) <Martin.Lundberg.Extern@jsmd-group.com>
2020-04-30 22:25:06 +0200

    use data binding for the nickname

--- app/aboutme/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/aboutme/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.aboutme
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.aboutme.databinding.ActivityMainBinding
 
@@ -15,10 +14,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("Aleks Haecky")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         //findViewById<Button>(R.id.done_button).setOnClickListener{
         //    addNickname(it)
@@ -34,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
 
         binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
             nicknameText.text = binding.nicknameEdit.text
             invalidateAll()
             nicknameEdit.visibility = View.GONE
