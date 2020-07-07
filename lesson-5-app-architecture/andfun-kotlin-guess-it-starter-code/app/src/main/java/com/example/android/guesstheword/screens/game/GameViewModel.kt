bafe296fff5c5652b087e302eaf7379e7e3fd598
molundb <martin_o_lundberg@hotmail.com>
2020-07-07 21:38:00 +0200

    encapsulate word and score

--- lesson-5-app-architecture/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ lesson-5-app-architecture/andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,14 +1,19 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
-    var word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
 
-    var score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
 
     private lateinit var wordList: MutableList<String>
 
@@ -16,7 +21,7 @@ class GameViewModel : ViewModel() {
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
-        score.value = 0
+        _score.value = 0
     }
 
     override fun onCleared() {
@@ -62,17 +67,17 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
      fun onSkip() {
-        score.value = (score.value)?.minus( 1)
+        _score.value = (score.value)?.minus( 1)
         nextWord()
     }
 
      fun onCorrect() {
-         score.value = (score.value)?.plus(1 )
+         _score.value = (score.value)?.plus(1 )
         nextWord()
     }
 }
